40cdbe5dde65f2d4d7383e21e5af9edd9fecb3a3
Anjali Bathula <anjalibathula1@gmail.com>
2021-02-18 03:55:37 -0800

    base UI login and signup screens

--- WestwoodWalks/screens/LoginScreen.js
+++ WestwoodWalks/screens/LoginScreen.js
@@ -1,18 +1,70 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView } from 'react-native';
 import {TouchableOpacity} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { TextInput } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../assets/logo.png")}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Username or Email"
+        keyboardType={'email-address'}
+        onSubmitEditing={()=> this.password.focus()}
+        returnKeyType="next"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        ref={(input)=>this.password = input}
+      />
+      <TouchableOpacity>
+        <Text style={styles.login}>Login</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C6E2E8',
+    alignItems: 'center',
+    justifyContent: "center"
+  },
+  logo :{
+    width: 225,
+    height: 225,
+    marginTop: '-30%'
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    height: 50,
+    marginVertical: 10,
+    padding: 10,
+    justifyContent: "center"
+  },
+  login: {
+    backgroundColor: 'white',
+    color: 'black',
+    marginTop: '10%',
+    width: 110,
+    borderRadius: 12,
+    overflow: 'hidden',
+    textAlign: 'center',
+    fontSize: 20,
+    padding: '2%'
+  }
+})
 
 export default LoginScreen;
--- WestwoodWalks/screens/SignupScreen.js
+++ WestwoodWalks/screens/SignupScreen.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView, TextInput} from 'react-native';
 import {TouchableOpacity} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,10 +9,71 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const SignupScreen = ({ navigation }) => {
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Signup Screen</Text>
-        </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/logo.png")}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          keyboardType={'email-address'}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          keyboardType={'email-address'}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          keyboardType={'email-address'}
+          secureTextEntry
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Repeat Password"
+          secureTextEntry
+        />
+        <TouchableOpacity>
+        <Text style={styles.login}>Sign Up</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#C6E2E8',
+      alignItems: 'center',
+      justifyContent: "center"
+    },
+    logo :{
+      width: 225,
+      height: 225,
+      marginTop: '-30%'
+    },
+    input: {
+      width: "80%",
+      borderWidth: 1,
+      height: 50,
+      marginVertical: 10,
+      padding: 10,
+      justifyContent: "center"
+    },
+    login: {
+      backgroundColor: 'white',
+      color: 'black',
+      marginTop: '10%',
+      width: 110,
+      borderRadius: 12,
+      overflow: 'hidden',
+      textAlign: 'center',
+      fontSize: 20,
+      padding: '2%'
+    }
+  })
+  
 
 export default SignupScreen;
