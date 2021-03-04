e9cac667e8f888a023c09f9a6d906f52aaa96a13
Mirayna Mckinney <miraynamckinney77@gmail.com>
2021-03-03 17:05:43 -0800

    Updated Maps

--- WestwoodWalks/screens/MapScreen.js
+++ WestwoodWalks/screens/MapScreen.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View, Alert, TextInput, Image} from 'react-native';
 import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import { Marker } from "react-native-maps";
 import { Text } from 'react-native';
 import styles from './HomeScreen.js'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import Buttons from '../styles/Buttons.js'
+import clocation from '../assets/clocation.png'
 
+const { width, height } = Dimensions.get('window');
 const LATITUDE = 34.06279;
 const LONGITUDE = -118.44390;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = .05;
-
+const DATA = [
+  {
+    title: 'Target',
+    data: {latitude:34.06277, longitude:-118.44392},
+  },
+  {
+    title: 'Inverted Fountain',
+    data: {latitude:34.07032, longitude:-118.44074},
+  },
+  {
+    title: 'Royce Hall',
+    data: {latitude:34.07301, longitude:-118.44229},
+  },
+  {
+    title: 'Charles Young Research Library',
+    data: {latitude:34.07523, longitude:-118.44144},
+  },
+];
 
 const GOOGLE_MAPS_APIKEY = 'AIzaSyBx4y6okTMakFLVwR5PKVN9kyqpbJFykrE';
 
@@ -21,14 +41,15 @@ export default class MapScreen extends Component {
     super(props);
 
     this.state = {
+      startValue: 'Start',
+      initialCoords:[
+        {latitude:34.073026, longitude:-118.465619},
+        {latitude:34.067223, longitude:-118.410851}
+      ],
       coordinates: [
         {
           latitude: 34.06279,
           longitude: -118.44390,
-        },
-        {
-          latitude: 34.06241,
-          longitude: -118.44375,
         },
       ],
       clocation: {
@@ -36,12 +57,16 @@ export default class MapScreen extends Component {
         longitude:-118.44524,
       },
       dur: null,
-      dis: null
+      dis: null,
+      saveWalk:{
+      startingLocation: null,
+      destinationLocation: null,
+      forZoom: { distance: Number, duration: Number, coordinates: [] }
+    }
     };
-
     this.mapView = null;
   }
-
+  
   onMapPress = (e) => {
     this.setState({
       coordinates: [
@@ -50,18 +75,47 @@ export default class MapScreen extends Component {
       ],
     });
   }
-  onRoutePress = (long, lat ) => {
+  onSaveWalk = () =>{
     this.setState({
-      coordinates: [
-        {latitude: lat, longitude: long},
-        this.state.clocation
-      ],
-    });
+      saveWalk:{
+        startingLocation:this.state.coordinates[1],
+        destinationLocation: this.state.coordinates[0]},
+    })
+    if(this.state.startValue=='Start'){
+      this.setState({
+        startValue:'Stop'
+      });
+      this.mapView.fitToCoordinates(this.state.forZoom.coordinates,{
+        edgePadding: {
+          right: (width / 20),
+          bottom: (height / 20),
+          left: (width / 20),
+          top: (height / 20),
+        }
+        }
+      );
+    }
+    else{
+      this.setState({
+        startValue:'Start'
+      });
+      this.mapView.fitToCoordinates(this.state.initialCoords,{
+        edgePadding: {
+          right: width,
+          bottom: height,
+          left: width,
+          top: height
+        }
+        }
+      );
+    }
   }
 
   componentDidMount() {
     this.currentLocation();
     this.intervalID = setInterval(this.currentLocation.bind(this), 1000);
+    this.setState({coordinates: [...this.state.coordinates,
+      this.state.clocation]})
   }
 
   componentWillUnmount() {
@@ -94,6 +148,7 @@ export default class MapScreen extends Component {
           longitudeDelta: LONGITUDE_DELTA,
         }}
         style={StyleSheet.absoluteFill}
+        zoomEnabled = {true}
         ref={c => this.mapView = c}
         onPress={this.onMapPress}
       >
@@ -112,35 +167,37 @@ export default class MapScreen extends Component {
               console.log(`Started routing between "${params.origin}" and "${params.destination}"`);
             }}
             onReady={result => {
-              console.log(`Distance: ${result.distance} km`)
-              console.log(`Duration: ${result.duration} min.`)
               this.setState({
                 dis: result.distance,
-                dur: result.duration
+                dur: result.duration,
+                forZoom: result
               })
-            }}
-            onError={(errorMessage) => {
             }}
           />
         )}
-        <Marker coordinate={{latitude: this.state.clocation.latitude, longitude: this.state.clocation.longitude}} />
+        <Marker coordinate={{latitude: this.state.clocation.latitude, longitude: this.state.clocation.longitude}}/>
       </MapView>
       </View>
+      <TouchableOpacity
+        onPress={ this.onSaveWalk }
+      >
+      <Text style={Buttons.brownbutton}>{this.state.startValue}</Text>
+      </TouchableOpacity>
       </View>
       <View>
         <Text>
-          Premade Routes
+          Popular Routes
         </Text>
       </View>
       <View>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => { this.setState({ coordinates:[{latitude:34.07032, longitude:-118.44074}, this.state.clocation]}) }}>
           <Text>
             The Inverted Fountain
           </Text>
         </TouchableOpacity>
       </View>
       <View>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => { this.setState({ coordinates:[{latitude:34.06277, longitude:-118.44392}, this.state.clocation]}) }}>
           <Text>
             Target
           </Text>
